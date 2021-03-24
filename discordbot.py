c912530f57829bd1541b24992431bd5cd5349b3d
elis-alubus <51432679+elis-alubus@users.noreply.github.com>
2021-03-24 19:10:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,5 @@
+from discord.permissions import PermissionOverwrite
+import discord
 from discord.ext import commands
 import os
 import traceback
@@ -16,8 +18,28 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+async def create(ctx):
+    roles = ctx.guild.roles
+    categories = ctx.guild.categories
+    category_names = [c.name for c in categories]
+    del roles[0]
+    role_names = [i.name for i in roles]
+    roles_dic = dict(zip(roles, role_names))
+    guild = ctx.guild
+    for key, val in roles_dic.items():
+        if val not in category_names:
+            overwrites = {
+                ctx.guild.default_role: discord.PermissionOverwrite(
+                    read_messages=False
+                ),
+                key: discord.PermissionOverwrite(read_messages=True),
+            }
+            category = await guild.create_category(val)
+            await category.create_text_channel("mtg")
+            await category.create_text_channel(name="雑談", overwrites=overwrites)
+            await category.create_text_channel(name="反省会", overwrites=overwrites)
+            await category.create_voice_channel("会議")
+
 
 
 bot.run(token)
